cf3f1c02df5f619cfa99e9841087777f09ca5745
harshita21-06 <76273784+harshita21-06@users.noreply.github.com>
2024-07-19 15:44:21 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3)
+console.log("Hiii")
+
+console.log(2+2); console.log("Hello") // Bad Practice, code should be readable
+
+// ECMA Script - for documentation
+// npn 
+
+let name = "Hey guys" // string
+let age = 22 // number
+let isTrue = false; // boolean
+
+
+// number ==> 2 to power 53
+// bigint
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value. eg :- let temperature = null (a value)
+// undefined ==> only variable, no declaration. eg :- let state (no value assign)
+// symbol ==> to find uniqueness 
+
+
+// object 
+
+console.log(typeof "Heeeeheee"); // string
+console.log(typeof age); // number
+console.log(typeof null); // object 
+console.log(typeof undefined) // undefined
