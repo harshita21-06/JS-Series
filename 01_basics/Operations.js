d8bb278a3e29a9bd80ceb629ed3eecbcd16fd4ce
harshita21-06 <76273784+harshita21-06@users.noreply.github.com>
2024-08-01 08:10:22 +0000

    operations

---
+++ 01_basics/Operations.js
@@ -0,0 +1,30 @@
+/*
+let value = 3;
+let negvalue = -value;
+console.log(negvalue); //-3
+*/
+
+/*
+console.log("1" + 2);  //12
+console.log(1 + "2");   //12
+console.log("1" + "2");   //12
+console.log(1 + 2);    //3
+*/
+
+/*
+console.log("1" + 2 + 2);  //122
+console.log(1 + 2 + "2");  //32
+*/
+
+// NOTE :- Use Opeartions with parentheses
+// + --> Can be used to do iterative conversions
+
+/*
+console.log(true); //true
+console.log(+true); //1
+console.log(true+) //error (Works acc. to Operators precedence)
+console.log("");
+console.log(+"");  //0
+*/
+
+
